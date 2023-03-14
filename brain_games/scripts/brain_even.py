c4ff25845fadc4e2e56eab9b07bd3b6f7805816f
Ruslan Nikolaenko <eventautotest@gmail.com>
2023-03-14 14:31:59 +0500

    Fifth  commit

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,23 +1,27 @@
 import random
 from brain_games.cli import welcome_user, greetings
 
+
+def is_even(num):
+    return num % 2 == 0
+
+
 def even_game():
     greetings()
     user_name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     right_answer = 0
     while right_answer < 3:
-        num = random.randint(1,100)
+        num = random.randint(1, 100)
         print(f'Question: {num}')
         answer = input('Your answer: ')
-        correct_answer = 'yes' if num % 2 == 0 else 'no'
-        if answer != correct_answer:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {user_name}!")
-        elif answer not in ('yes', 'no'):
-            print(f"Incorrect input, please enter 'yes' or 'no', {user_name}")
-        else:
+        if answer == 'yes' and is_even(num) or answer == 'no' and not is_even(num):
             print('Correct!')
             right_answer += 1
+        else:
+            correct_answer = 'yes' if is_even(num) else 'no'
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {user_name}!")
+            return
     print(f'Congratulations, {user_name}!')
 
 
