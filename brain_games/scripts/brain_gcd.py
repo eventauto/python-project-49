4b9c578bce56987cf409e4b5ffb3e35c6f5199bd
Ruslan Nikolaenko <eventautotest@gmail.com>
2023-03-14 15:52:39 +0500

    Im dispolied at this moment

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -21,6 +21,7 @@ def play_game():
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {user_name}!")
+            return
     print(f"Congratulations, {user_name}!")
 
 def main():
