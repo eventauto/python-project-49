8d6e3b460a49e42945b9ac1827cc5e8dadb6d724
rusla <eventaustotest@gmail.com>
2023-04-10 20:59:06 +0500

    Linter fixes

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,7 +1,7 @@
 import random
 
-task_description = '''Answer "yes" if given number is prime.
-Otherwise answer "no".'''
+task_description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
 
 
 MIN_NUMBER = 1
